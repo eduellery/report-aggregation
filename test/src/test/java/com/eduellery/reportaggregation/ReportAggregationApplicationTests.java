977be6f43cb48722705de66359bd0f380eb51c19
Eduardo Ellery <eduellery@gmail.com>
2022-02-13 18:32:32 +0100

    Add test module

---
+++ test/src/test/java/com/eduellery/reportaggregation/ReportAggregationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.eduellery.reportaggregation;
+
+import org.junit.jupiter.api.Test;
+
+class ReportAggregationApplicationTests {
+
+	ReportAggregationApplication app = new ReportAggregationApplication();
+
+	@Test
+	void testAppMain() {
+		app.main(new String[0]);
+	}
+
+}
